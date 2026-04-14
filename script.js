b80cfadafbb00d10148b80363ad9ee22e6aeb94c
Pincheng Yao <pinchengyao1201@gmail.com>
2026-04-14 04:20:27 +0000

    HTML Wireframe with a bit of js to get name and starting balance

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// add javascript here
+//username = prompt("Enter name here:");
+//document.getElementById("Webtitle").textContent = username + "'s Balance Tracker";
+//balance = parseInt(prompt("What is your current balance"))
+//while (isNaN(balance)) { 
+//    alert("Invalid Number")
+//    balance = parseInt(prompt("What is your current balance"))
+//}
+//document.getElementById("currentBal").textContent = "Current Balance: $" + balance;
