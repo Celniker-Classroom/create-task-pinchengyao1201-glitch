f33b0b77ca8c63508f415f13fead438a38ae2392
Pincheng Yao <pinchengyao1201@gmail.com>
2026-04-14 20:51:11 +0000

    Started with transaction and recent transaction update. Got transname and type and amt from clicking on button

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-//username = prompt("Enter name here:");
-//document.getElementById("Webtitle").textContent = username + "'s Balance Tracker";
-//balance = parseInt(prompt("What is your current balance"))
-//while (isNaN(balance)) { 
-//    alert("Invalid Number")
-//    balance = parseInt(prompt("What is your current balance"))
-//}
-//document.getElementById("currentBal").textContent = "Current Balance: $" + balance;
+username = prompt("Enter name here:");
+document.getElementById("Webtitle").textContent = username + "'s Balance Tracker";
+balance = parseInt(prompt("What is your current balance"));
+while (isNaN(balance)) { 
+    alert("Invalid Number")
+    balance = parseInt(prompt("What is your current balance"))
+};
+
+document.getElementById("currentBal").textContent = "Current Balance: $" + balance;
+
+nameList = []
+amtList = []
+typeList = []
+
+function recentTransUpd() { 
+    for (i=0; i<3; i++){
+        document.getElementById("recent" + i).textContent = nameList[i] + "," + amtList[i] + "," + transType[i];
+    }
+}
+
+document.getElementById("addTrans").addEventListener("click", function () {
+    transName = document.getElementById("transactionName").value
+    nameList.push(transName)
+    transAmt = document.getElementById("transactionAmt").value
+    amtList.push(transAmt)
+    transType = document.getElementById("transactionType").options[document.getElementById("transactionType").selectedIndex].value
+    typeList.push(transType)
+    recentTransUpd()
+})
